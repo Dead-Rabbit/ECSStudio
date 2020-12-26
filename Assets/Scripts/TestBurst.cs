22dbff652b9efff9bad20d89fdaf099dbe3d03a6
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2020-12-26 11:04:49 +0800

    update

---
+++ Assets/Scripts/TestBurst.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class TestBurst : MonoBehaviour
+{
+    void Update()
+    {
+        RunTestBurst();
+    }
+
+    [BurstCompile]
+    void RunTestBurst()
+    {
+        NativeArray<Vector3> velocity = new NativeArray<Vector3>(1000, Allocator.Persistent);
+        Profiler.BeginSample("NativeArray");
+        for (var i = 0; i < velocity.Length; i++) {
+            velocity[i] = Vector3.zero;
+        }
+        Profiler.EndSample();
+        velocity.Dispose();
+
+        Vector3[] velocity2 = new Vector3[1000];
+        Profiler.BeginSample("Vector3");
+        for (var i = 0; i < velocity2.Length; i++) {
+            velocity2[i] = Vector3.zero;
+        }
+        Profiler.EndSample();
+    }
+}
