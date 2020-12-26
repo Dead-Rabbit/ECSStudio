2b0b51b02d05b5cefd9bd6369dfd2dcaa997341a
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2020-12-26 22:25:54 +0800

    update in mine pc

--- Assets/Scripts/test/Testing.cs
+++ Assets/Scripts/test/Testing.cs
@@ -10,6 +10,8 @@ namespace test
 {
     public class Testing : MonoBehaviour
     {
+        [SerializeField] private Mesh _mesh;
+        [SerializeField] private Material _material;
         private void Start()
         {
             EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
@@ -17,12 +19,13 @@ namespace test
             EntityArchetype entityArchetype = entityManager.CreateArchetype(
                 typeof(LevelComponent),
                 typeof(Translation),
-                typeof(RenderMesh)
+                typeof(RenderMesh),
+                typeof(LocalToWorld)
                 );
-
             // 此处的NativeArray仅用于给Entities赋值，所以使用Allocator的Temp存储类型
             NativeArray<Entity> entitiesArray = new NativeArray<Entity>(1, Allocator.Temp);
             entityManager.CreateEntity(entityArchetype, entitiesArray);
+            
             for (var i = 0; i < entitiesArray.Length; i++) {
                 Entity entity = entitiesArray[i];
                 entityManager.SetComponentData(entity, new LevelComponent{level = Random.Range(10, 20)});
