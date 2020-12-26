78a96075c17881bcc8e27352ef01ec919bcdf1b6
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2020-12-26 12:23:14 +0800

    update

---
+++ Assets/Scripts/component/LevelComponent.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace component
+{
+    public struct LevelComponent : IComponentData
+    {
+        public float level;
+    }
+}
---
+++ Assets/Scripts/test/Testing.cs
@@ -0,0 +1,17 @@
+﻿using component;
+using UnityEngine;
+using Unity.Entities;
+
+namespace test
+{
+    public class Testing : MonoBehaviour
+    {
+        private void Start()
+        {
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            Entity entity = entityManager.CreateEntity(typeof(LevelComponent));
+            
+            entityManager.SetComponentData(entity, new LevelComponent{level = 10});
+        }
+    }
+}
