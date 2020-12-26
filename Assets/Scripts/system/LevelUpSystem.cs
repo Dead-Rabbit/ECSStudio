db0f9f1cbb92783be907643f2273bff311caf976
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2020-12-26 14:40:43 +0800

    update in mine pc

---
+++ Assets/Scripts/system/LevelUpSystem.cs
@@ -0,0 +1,16 @@
+﻿using component;
+using Unity.Entities;
+
+namespace system
+{
+    public class LevelUpSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((ref LevelComponent levelComponent) =>
+            {
+                levelComponent.level += 1f * Time.DeltaTime;
+            });
+        }
+    }
+}
